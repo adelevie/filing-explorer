d6d81fe73804a62d7e02dc3429010cd6b6f36307
Alan deLevie <adelevie@gmail.com>
2017-03-30 22:26:11 -0400

    Added filings app to installed apps

--- filingexplorer/settings.py
+++ filingexplorer/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'filings.apps.FilingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
