b98fa297288ad5f07af421e166d243a906e0ad83
Alan deLevie <adelevie@gmail.com>
2017-04-27 20:48:08 -0400

    Added MVP of index and detail filings views

--- filings/views.py
+++ filings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from filings.models import Filing
+
+def index(request):
+    filings = Filing.objects.all()
+    context = {
+        'filings': filings
+    }
+    return render(request, 'filings/index.html', context)
+
+def detail(request, filing_id):
+    filing = get_object_or_404(Filing, pk=int(filing_id))
+    context = {
+        'filing': filing
+    }
+    return render(request, 'filings/detail.html', context)
